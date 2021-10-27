261f51909f4c99a98548625937a044fe2d5d2d42
Esun Kim <veblush@google.com>
2021-10-27 16:31:59 -0700

    cpolicy = const

--- e2e-examples/gcs/benchmark/main.cc
+++ e2e-examples/gcs/benchmark/main.cc
@@ -17,7 +17,6 @@
 #include "absl/flags/parse.h"
 #include "absl/strings/str_format.h"
 #include "absl/time/time.h"
-
 #include "channel_creator.h"
 #include "channel_policy.h"
 #include "print_result.h"
@@ -30,7 +29,8 @@ using std::string;
 
 ABSL_FLAG(string, access_token, "", "Access token for auth");
 ABSL_FLAG(string, network, "default", "Network path (default, cfe, dp)");
-ABSL_FLAG(bool, rr, false, "Use round_robin grpclb policy (otherwise pick_first)");
+ABSL_FLAG(bool, rr, false,
+          "Use round_robin grpclb policy (otherwise pick_first)");
 ABSL_FLAG(bool, td, false, "Use Traffic Director");
 ABSL_FLAG(string, operation, "read", "Operation type (read, write)");
 ABSL_FLAG(string, host, "dns:///storage.googleapis.com:443", "Host to reach");
@@ -53,7 +53,7 @@ ABSL_FLAG(bool, resumable, false, "Use resumable-write for writing");
 ABSL_FLAG(int, ctest, 0, "Test to get a list of peers from grpclb");
 ABSL_FLAG(int, threads, 1, "The number of threads running downloding objects");
 ABSL_FLAG(string, cpolicy, "perthread",
-          "Channel Policy (perthread, percall, pool, bpool, spool)");
+          "Channel Policy (perthread, percall, const, pool, bpool, spool)");
 ABSL_FLAG(
     int, carg, 0,
     "Parameter for cpolicy (e.g. pool uses this as the number of channels)");
@@ -69,8 +69,7 @@ std::shared_ptr<grpc::Channel> CreateBenchmarkGrpcChannel() {
   return CreateGrpcChannel(absl::GetFlag(FLAGS_host),
                            absl::GetFlag(FLAGS_access_token),
                            absl::GetFlag(FLAGS_network),
-                           absl::GetFlag(FLAGS_rr),
-                           absl::GetFlag(FLAGS_td));
+                           absl::GetFlag(FLAGS_rr), absl::GetFlag(FLAGS_td));
 }
 
 int main(int argc, char** argv) {
@@ -103,7 +102,9 @@ int main(int argc, char** argv) {
 
   // Initializes a gRPC channel pool.
   std::shared_ptr<StorageStubProvider> stub_pool;
-  if (cpolicy == "pool") {
+  if (cpolicy == "const") {
+    stub_pool = CreateConstChannelPool(&CreateBenchmarkGrpcChannel);
+  } else if (cpolicy == "pool") {
     if (carg <= 0) {
       std::cerr << "Invalid carg: " << carg << std::endl;
       return 1;
